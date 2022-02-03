60c45175db3e2a5194ae00b31f856d9624e4c3e0
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-02-03 01:03:23 -0500

    Add support for google signin from configuration

--- Source/IdentityServerHost/Program.cs
+++ Source/IdentityServerHost/Program.cs
@@ -106,8 +106,8 @@ builder.Services
                     // register your IdentityServer with Google at https://console.developers.google.com
                     // enable the Google+ API
                     // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
+                    options.ClientId = builder.Configuration["Authentication__Google__ClientId"];
+                    options.ClientSecret = builder.Configuration["Authentication__Google__ClientSecret"];
                 }).Services
         .AddControllersWithViews();
 
